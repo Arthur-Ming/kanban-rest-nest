9a3e7f83cfd424f7a0366c86286a1121127d4a4c
Artur Mingazhov <arthurming7@gmail.com>
2023-04-25 16:36:33 +0300

    feat: add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import config from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
 })
 export class AppModule {}
