aedbe426b2cf10f84b3b71842328bd4cfb0511e3
Artur Mingazhov <arthurming7@gmail.com>
2023-04-25 16:38:30 +0300

    feat: connect to mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from './config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -8,6 +9,12 @@ import config from './config';
       load: [config],
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('mongodb').uri,
+      }),
     }),
   ],
 })
